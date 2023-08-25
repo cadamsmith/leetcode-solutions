214044a0ba8d63ced27b93d066814b0d1bc1d132
cadamsmith <cadamsmith.dev@gmail.com>
2023-08-24 19:52:51 -0500

    stuff

---
+++ csharp/src/Solutions.Lib/0001_TwoSum/TwoSumSolutionB.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions.Lib;
+
+public class TwoSumSolutionB : TwoSumSolution
+{
+    protected override int[] TwoSum(int[] nums, int target)
+    {
+        int count = nums.Length;
+
+        for (int i = 0; i < count - 2; i++)
+        {
+            for (int j = i + 1; j < count; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return new int[] { count - 2, count - 1 };
+    }
+}
