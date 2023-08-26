5de57300b005a904c01ebfbe71579c7137b3a59a
cadamsmith <cadamsmith.dev@gmail.com>
2023-08-26 11:14:26 -0500

    finished my first AddTwoNumbers solution

---
+++ csharp/Solutions.AdHocTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using Solutions.Lib;
+
+try
+{
+	// Arrange
+	ListNode l1 = ListNode.WithValues(2, 4, 3);
+	ListNode l2 = ListNode.WithValues(5, 6, 4);
+	AddTwoNumbersSolutionA solution = new();
+
+	// Act
+	ListNode result = (ListNode) solution.Execute(l1, l2);
+	int[] values = result.GetValues().ToArray();
+}
+catch (Exception e)
+{
+	Console.WriteLine(e.ToString());
+}
---
+++ csharp/tests/Solutions.Tests/0002_AddTwoNumbers/AddTwoNumbersSolutionATests.cs
@@ -0,0 +1,9 @@
+﻿using Solutions.Lib;
+
+namespace Solutions.Tests;
+
+public class AddTwoNumbersSolutionATests : BaseAddTwoNumbersSolutionTests<AddTwoNumbersSolutionA>
+{
+	public AddTwoNumbersSolutionATests(BaseSolutionFixture fixture)
+		: base(fixture) { }
+}
