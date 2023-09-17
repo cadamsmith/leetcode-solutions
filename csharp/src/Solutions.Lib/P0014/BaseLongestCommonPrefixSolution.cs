6ed0dec3f7e251527a80e369cbd65103a3100d7e
cadamsmith <cadamsmith.dev@gmail.com>
2023-09-17 00:28:30 -0500

    a longest common prefix solution in c#

---
+++ csharp/src/Solutions.Lib/P0014/BaseLongestCommonPrefixSolution.cs
@@ -0,0 +1,13 @@
+﻿namespace Solutions.Lib.P0014;
+
+public abstract class BaseLongestCommonPrefixSolution : BaseSolution
+{
+    protected override object Solve(object[] parameters)
+    {
+        string[] strs = (string[]) parameters[0];
+
+        return LongestCommonPrefix(strs);
+    }
+
+    protected abstract string LongestCommonPrefix(string[] strs);
+}
